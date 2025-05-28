388528ae7ebd944bc95f769452c34b6962aa8fdc
mariasemangela <140639757+Ange2008@users.noreply.github.com>
2025-05-28 14:15:51 -0300

    0000

--- main.js
+++ main.js
@@ -11,4 +11,24 @@ for (let i=0; i < botoes.length; i++){
     }
 }
 const contadores = document.querySelectorAll("contador");
-const tempoObjetivo1 = new Date ("2025-07-13T"00:00:00):
+const tempoObjetivo1 = new Date ("2025-07-13T00:00:00");
+const tempoObjetivo2 = new Date ("")
+const tempoObjetivo3 = new Date ("2025-10-08T00:00:00");
+const tempoObjetivo4 = new Date ("2026-07-15T00:00:00");
+const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
+function atualizaCronometro (){
+    for (let i= 0; i < contadores.length; i++){
+        document.getElementById ("dias"+i).textContent = caculaTempo(tempos [i])[0];
+        document.getElementById ("horas"+i).textContent = caculaTempo(tempos [i])[1];
+        document.getElementById ("min"+i).textContent = caculaTempo(tempos [i])[2];
+        document.getElementById ("seg"+i).textContent = caculaTempo(tempos [i])[3];
+    }
+    for (let i = 0; i < contadores.length; i++){
+        //contadores[i].textContent = calculaTempo (tempos [i]);
+    }
+}
+function comecaCronometro (){
+    atualizaCronometro ();
+    setInterval (atualizaCronometro, 1000);
+}
+comecaCronometro ();
