84aacae67ce92c42f586d669d1cb2d805ecf4a49
mariasemangela <140639757+Ange2008@users.noreply.github.com>
2025-06-11 07:40:35 -0300

    0000

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ for (let i=0; i < botoes.length; i++){
 }
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date ("2025-07-13T00:00:00");
-const tempoObjetivo2 = new Date ("")
+const tempoObjetivo2 = new Date ("2026-02-27T00:00:00");
 const tempoObjetivo3 = new Date ("2025-10-08T00:00:00");
 const tempoObjetivo4 = new Date ("2026-07-15T00:00:00");
 const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
